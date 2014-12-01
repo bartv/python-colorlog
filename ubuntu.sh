75ff299a582c37c8cbe7b70efbe3a3a6fa4e0d4e
Bart Vanbrabant <bart@vanbrabant.eu>
2014-12-01 21:47:24 +0100

    Update

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,9 +4,9 @@ NAME=colorlog
 VERSION=$(cat debian/changelog | grep $NAME | sed -e "s/$NAME (\([^-]*\)-.*) .*/\1/g" | head -n 1)
 
 echo "Building version $VERSION"
-TAR=$NAME_$VERSION.orig.tar.gz
+TAR="${NAME}_${VERSION}.orig.tar.gz"
 
-curl https://pypi.python.org/packages/source/c/colorlog/colorlog-$VERSION.tar.gz > $NAME_$VERSION.orig.tar.gz
+curl https://pypi.python.org/packages/source/c/colorlog/colorlog-$VERSION.tar.gz > $TAR
 
 tar xvfz $TAR
 cp -a debian $NAME-$VERSION
